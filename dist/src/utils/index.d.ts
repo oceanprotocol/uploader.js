050175adc30a36e81cb383a2020cd186124b85fb
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-07-12 19:23:46 +0300

    Fixing linting in tests

---
+++ dist/src/utils/index.d.ts
@@ -0,0 +1,12 @@
+import { Signer } from 'ethers'
+/**
+ * @param {string} quoteId - The quote ID.
+ * @param {Signer} signer The signer object.
+ * @param {number} nonce - A timestamp (must be higher than the previously stored nonce for the user).
+ * @returns {Promise<string>} - A promise that resolves to the signed hash.
+ */
+export declare const getSignedHash: (
+  signer: Signer,
+  quoteId: string,
+  nonce: number
+) => Promise<string>
