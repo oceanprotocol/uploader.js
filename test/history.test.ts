d6c83adfffd1e88ad054730b6c808b1a22946282
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-08-11 15:19:07 +0300

    Adding a test for the gethistory endpoint

---
+++ test/history.test.ts
@@ -0,0 +1,26 @@
+import { ethers, JsonRpcProvider } from 'ethers'
+import dotenv from 'dotenv'
+
+import { DBSClient } from '../src/index'
+
+dotenv.config()
+
+describe('Get History test', () => {
+  // Set up a new instance of the DBS client
+  const provider = new JsonRpcProvider(process.env.RPC_URL, 80001)
+  // Private key account needs to have both MATIC and Wrapped Matic for testing
+  const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  const client = new DBSClient(process.env.DBS_API_URL, process.env.DBS_ACCOUNT, signer)
+
+  describe('Testing the getHistory endpoint', async function () {
+    it('should return the history for the signer', async () => {
+      let result
+      try {
+        result = await client.getHistory()
+        console.log('result', result)
+      } catch (error) {
+        console.log('error', error)
+      }
+    })
+  })
+})
